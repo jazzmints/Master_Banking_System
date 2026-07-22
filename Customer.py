d5b2cc3d94e46a1603f8be0c7f6cf89fa1ac43da
SamieJay <samisjam.sij@gmail.com>
2026-07-22 10:17:15 -0400

    updating customer

--- Customer.py
+++ Customer.py
@@ -1,2 +1,18 @@
 class Customer:
-    
+
+    def __init__(self, name, account_number, balance):
+        self.name = name
+        self.account_number = account_number
+        self.balance = balance
+
+    def show(self):
+        print("Enter Name: ", self.name)
+        print("Account_Number: ", self.account_number)
+        print("Balance: ", self.balance)
+        print()
+
+customer1 = Customer()
+customer2 = Customer()
+customer3 = Customer()
+
+show_customer()
