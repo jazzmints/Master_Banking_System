80bba2341ba7d82cbb1a593d781e884e30b27898
Zay <jserdriguez06@gmail.com>
2026-07-22 10:52:24 -0400

    made account number input a int

--- CreateCustomer.py
+++ CreateCustomer.py
@@ -4,7 +4,7 @@ class CreateCustomer:
 
     def create_customer(self, bank):
         name = input("Enter customer name:")
-        account_number = input("Enter account number:")
+        account_number = input(int("Enter account number:"))
         balance = float(input("Enter starting balance:"))
 
         bank.add_customer(name, account_number, balance)
