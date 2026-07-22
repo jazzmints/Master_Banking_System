63e54939c9820216d5c0e0b47d2a2b3b9cb066db
Zay <jserdriguez06@gmail.com>
2026-07-22 10:32:58 -0400

    .

---
+++ CreateCustomer.py
@@ -0,0 +1,12 @@
+from Bank import Bank
+
+class CreateCustomer:
+
+    def create_customer(self, bank):
+        name = input("Enter customer name:")
+        account_number = input("Enter account number:")
+        balance = float(input("Enter starting balance:"))
+
+        bank.add_customer(name, account_number, balance)
+
+        print("Customer created successfully!")
