05a9c865c13c03c102ca61bb23811c5543b453d0
haziellopez22 <haziel.lopez189@outlook.com>
2026-07-22 10:38:35 -0400

    hthgrt

---
+++ Bank.py
@@ -0,0 +1,26 @@
+# Bank
+from Customer import Customer
+
+# Defining Bank
+class Bank:
+    def __init__(self, customers):
+        self.customers = []
+
+    def add_customer(self):
+        for customer in self.customers:
+            self.customers.append(customer)
+
+    def find_customer(self):
+        for customer in self.customers:
+            if customer in self.customers:
+                print(customer)
+
+    def remove_customer(self):
+        for customer in self.customers:
+            if customer in self.customers:
+                self.customers.remove(customer)
+
+    def get_customer(self):
+        for customer in self.customers:
+            if customer in self.customers:
+                self.customers .index(customer)
